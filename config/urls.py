fc8d16646c9ad508e7b0ea7f1493de643d41d4a8
SORARAwo4649 <k11274649@icloud.com>
2021-01-30 11:52:19 +0900

    ヘルプ用アプリの作成

--- config/urls.py
+++ config/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("myhealthapp/", include("myhealthapp.urls")),
+    path("myhelps/", include("myhelps.urls")),
 ]
