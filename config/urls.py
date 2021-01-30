88e38190caf7dafaa927d7d19b8933605ef639f8
SORARAwo4649 <k11274649@icloud.com>
2021-01-30 14:33:28 +0900

    ログインページの実装

--- config/urls.py
+++ config/urls.py
@@ -4,5 +4,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("myhealthapp/", include("myhealthapp.urls")),
+    path("myhealthapp/", include("django.contrib.auth.urls")),
     path("myhelps/", include("myhelps.urls")),
 ]
