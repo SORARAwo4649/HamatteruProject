8bb00d537bd3fb19b353193d61864bed12aa3a24
SORARAwo4649 <k11274649@icloud.com>
2021-02-09 19:58:32 +0900

    モデルフィールドの追加

--- myhealthapp/models.py
+++ myhealthapp/models.py
@@ -1,14 +1,23 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 
 class List(models.Model):
-    date = models.DateField()
-    go_to_bed = models.TimeField()
-    wakeup = models.TimeField()
-    short_comment = models.TextField(max_length=200)
+    date = models.DateField("日付")
+    go_to_bed = models.TimeField("就寝時間")
+    wakeup = models.TimeField("起床時間")
+    short_comment = models.TextField("感想", max_length=200)
 
     # 追加分①
-    sleep_time = models.TimeField(null=True)
+    sleep_time = models.TimeField("睡眠時間", null=True)
+
+    # 追加分②
+    sleep_quality = models.IntegerField(
+        "睡眠の質",
+        validators=[MinValueValidator(1), MaxValueValidator(10)],
+        null=True
+    )
+    staff_comment = models.TextField("スタッフコメント", max_length=200, null=True)
 
     def __str__(self):
         return self.date
