97399af34aa8d6ce65eec6e277c633b872d9ddae
SORARAwo4649 <k11274649@icloud.com>
2021-02-10 21:05:34 +0900

    入力フォームの修正

--- config/settings.py
+++ config/settings.py
@@ -3,7 +3,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -14,7 +13,6 @@ SECRET_KEY = 'r9g-n(iv=szll!!-y&4*7iwagc^fguxh&@6zy1_4yjvc(xgpg8'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -27,7 +25,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'myhealthapp.apps.MyhealthappConfig',
     'myhelps.apps.MyhelpsConfig',
+    'bootstrap_datepicker_plus',
+    'crispy_forms',
+    'django_filters',
+    'bootstrap4',
 ]
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -59,7 +62,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -69,7 +71,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -89,7 +90,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -102,7 +102,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
--- myhealthapp/forms.py
+++ myhealthapp/forms.py
@@ -1,10 +1,31 @@
 from django import forms
 
 from .models import List
+import bootstrap_datepicker_plus as datetimepicker
 
 
 class ListForm(forms.ModelForm):
+    """
+    入力の遊び
+    https://blog.narito.ninja/detail/83/
+    """
 
     class Meta:
         model = List
-        fields = ("date", "go_to_bed", "wakeup", "short_comment",)
+        fields = (
+            "date",
+            "go_to_bed",
+            "wakeup",
+            "short_comment",
+            "sleep_quality",
+            "staff_comment"
+        )
+        widgets = {
+            "date": datetimepicker.DatePickerInput(
+                format='%Y-%m-%d',
+                options={
+                    'locale': 'ja',
+                    'dayViewHeaderFormat': 'YYYY年 MMMM',
+                }
+            )
+        }
