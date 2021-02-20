cb8a9340f9e7c7b719fd505acb63d8328b5a1341
SORARAwo4649 <k11274649@icloud.com>
2021-02-20 19:00:54 +0900

    月が変わるときの対応をした

--- myhealthapp/writer/g_spread.py
+++ myhealthapp/writer/g_spread.py
@@ -32,22 +32,52 @@ class GSpreadWriter(models.Model):
             # ↓はバレても問題ないやつ
             SPREADSHEET_KEY = '1B2qqeqonfsLeFrgo_B_KuoOJfBe2JzxfXtt0sKVejPs'
 
-            # 共有設定したスプレッドシートのシート1を開く
-            worksheet = gc.open_by_key(SPREADSHEET_KEY).sheet1
-
+            print('諸々のデバッグ')
             date_into = model_set["date"].strftime('%Y/%m/%d')
+            print(date_into)
             go_to_bed_into = model_set["go_to_bed"].strftime('%Y/%m/%d %H:%M')
+            print(go_to_bed_into)
             wakeup_into = model_set["wakeup"].strftime('%Y/%m/%d %H:%M')
+            print(wakeup_into)
             sleep_quality_into = model_set["sleep_quality"]
+            print(sleep_quality_into)
             sleep_time_into = model_set["sleep_time"].strftime('%H:%M')
+            print(sleep_time_into)
             short_comment_into = model_set["short_comment"]
+            print(short_comment_into)
             staff_comment_into = model_set["staff_comment"]
-
-            # print(date_into)
+            print(staff_comment_into)
 
             # 日付ごとのセルの位置を決める前処理
             date_date = date_into.split("/")
+            checking_first_day = int(date_date[2])
             cell_position = str(int(date_date[2]) + 1)
+
+            # スプレッドシート全体のインスタンス化
+            ws = gc.open_by_key(SPREADSHEET_KEY)
+
+            # 毎月１日ならシートを新規作成
+            if checking_first_day == 1:
+                print('１日のデバッグ')
+                # まずはシートIDの取得
+                ws_id = ws.get_worksheet(0).id
+                print(ws_id)
+
+                # シートの名前を決める
+                new_sheet_name_set = \
+                    str(int(date_date[0])) + "_" + str(int(date_date[1]))
+
+                # templateをコピーする
+                print("ここらへんでエラー出てる気がする")
+                ws.duplicate_sheet(
+                    source_sheet_id=ws_id,
+                    new_sheet_name=new_sheet_name_set,
+                    insert_sheet_index=1
+                )
+
+            # シートのインスタンス化
+            print("ここらへんでエラー出てる気がする２")
+            worksheet = ws.get_worksheet(1)
 
             # 日付によってセルの位置を変更する処理
             date_cell_position = "A" + cell_position
