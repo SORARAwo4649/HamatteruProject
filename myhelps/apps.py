3af15199211d01ed359e3af9f1959152fdab2fe4
SORARAwo4649 <63144656+SORARAwo4649@users.noreply.github.com>
2021-02-21 20:12:49 +0900

    Delete myhelps directory

--- myhelps/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MyhelpsConfig(AppConfig):
-    name = 'myhelps'
--- myhelps/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = "myhelps"
-
-urlpatterns = [
-    path("", views.index, name="index"),
-]
