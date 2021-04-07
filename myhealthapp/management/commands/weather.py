5895bcfefd2d286177d1ce7b34cfd527ab969754
SORARAwo4649 <k11274649@icloud.com>
2021-04-07 18:38:00 +0900

    カスタムコマンドの作成

---
+++ myhealthapp/management/commands/weather.py
@@ -0,0 +1,49 @@
+from django.core.management.base import BaseCommand
+from myhealthapp.models import List
+from django.utils import timezone
+
+import requests
+from bs4 import BeautifulSoup
+import datetime
+import time
+
+
+class Command(BaseCommand):
+    help = 'Displays current time'
+
+    def handle(self, *args, **options):
+        print(datetime.datetime.today().strftime('%y/%m/%d'))
+        now = datetime.datetime.now()
+        year_buf = str(now.year)
+        print(year_buf)
+        month_buf = str(now.month)
+        print(month_buf)
+        day_buf = str(now.day)
+        print(day_buf)
+
+        # スクレイピング対象の URL にリクエストを送り HTML を取得する
+        url = 'https://www.data.jma.go.jp/obd/stats/etrn/view/daily_s1.php?prec_no=44&block_no=47662&'
+        url += "year=" + year_buf
+        url += '&month=' + month_buf
+        url += '&day=' + day_buf
+        url += '&view=p1'
+        print(url)
+        res = requests.get(url)
+
+        # レスポンスの HTML から BeautifulSoup オブジェクトを作る
+        soup = BeautifulSoup(res.content, 'html.parser')
+
+        # title タグの文字列を取得する
+        title_text = soup.find('title').get_text()
+        print(title_text)
+
+        cal_day = int(day_buf) + 4 - 2
+        print(cal_day)
+        test1 = \
+            soup.find(
+                "table",
+                {"id": "tablefix1"}
+            ).find_all("tr")[cal_day].find_all("td")[1]
+        test1_text = test1.get_text()
+        print(test1_text)
+        self.stdout.write("Success!!")
